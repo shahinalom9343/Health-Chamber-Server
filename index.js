d91c7b8dfd8adb94d6d529ff1488326f16b95b58
shahinalom9343 <cuet.shahin.eee12@gmail.com>
2024-07-25 04:15:19 -0700

    Created Basic Server with mongoDB setup. Also protect dotenv file and post registered Users

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.43teffq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    const userCollection = client.db("Health-Chamber").collection("users");
+
+    // post a new user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Health Chamber server is Running");
+});
+
+app.listen(port, async (req, res) => {
+  console.log(`Health Chamber Running on PORT : ${port}`);
+});
